d4c9526795b5af6294e3a1213dee8d5c4a1d33c2
Ari Umboro Seno <ariumboroseno@gmail.com>
2025-09-25 16:21:07 +0700

    rename vilages to villages

--- database/migrations/2025_09_24_043404_create_villages_table.php
+++ database/migrations/2025_09_24_043404_create_villages_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('vilages', function (Blueprint $table) {
+        Schema::create('villages', function (Blueprint $table) {
             $table->id();
             $table->string('province_code');
             $table->string('district_code');
