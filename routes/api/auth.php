376112311b47330c47f6666a68a314fb603a5f17
Ari Umboro Seno <ariumboroseno@gmail.com>
2025-11-25 15:16:51 +0700

    revisi logout

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -6,8 +6,5 @@ Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register'])->name('register');
     Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::get('check', [AuthController::class, 'checkToken'])->name('check-token');
-
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::post('logout', [AuthController::class, 'logout']);
-    });
+    Route::post('logout', [AuthController::class, 'logout']);
 });
